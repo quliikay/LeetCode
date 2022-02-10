273a13375a6e739ba0f56dd73fc9ab1a8d3c6b14
quliikay <879891091@qq.com>
2022-02-10 15:48:40 +0800

    2022/2/10

--- 1.两数之和.cpp
+++ 1.两数之和.cpp
@@ -10,15 +10,14 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> map;
-        for(int i=0;i<nums.size();i++){
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
             auto iter = map.find(target - nums[i]);
-            if(iter != map.end())
+            if (iter != map.end())
                 return {iter->second, i};
-            map[nums[i]] = i;
+            map.insert(pair<int, int>{nums[i], i});
         }
         return {};
     }
 };
 // @lc code=end
-
